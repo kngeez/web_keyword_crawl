56a0580d5fae05488af30338df7e9deac15b0d76
kng-linux <kng@raker.sea>
2014-11-07 07:03:41 -0800

    attempting to reduce redundancy in find_leads_page

--- ruby/find_leads_page.rb
+++ ruby/find_leads_page.rb
@@ -2,36 +2,51 @@
 # find leads page and get website if one exists
 
 require 'nokogiri'
-require 'openssl'
 require 'open-uri'
 require 'csv'
+
 
 allowed_domains = ""
 start_urls = ""
 
+#def add_to_csv(csv, line, id, allowed_domains, start_urls)
+#  puts "line in method:"
+#  puts line
+#  csv << [line, id]
+#  allowed_domains += "\'" + line.split("http://")[-1].split("www.")[-1].split("/")[0] + "\', "
+#  start_urls += "\'" + line + "\', "
+#end
+
+
 CSV.open("websites.csv", "wb") do |csv| # write to new csv file
   CSV.foreach("landing_pages_test.csv") do |row| # read from leadspages.csv
     line = row[0]
-    puts line
-    puts row[0]
-    csv << [line, row[1]]
 
+    csv << [line, row[1]]
     allowed_domains += "\'" + line.split("http://")[-1].split("www.")[-1].split("/")[0] + "\', "
     start_urls += "\'" + line + "\', "
+
+#    add_to_csv(csv, line, row[1], allowed_domains, start_urls)
 
     doc = Nokogiri::HTML(open(line))
     search_path = doc.xpath("/html/head/link[3]").to_s().split("http://")[-1]
 
     if search_path == "microsite.marchex.com/css/ms_base.css\">"
-      print "Leads page found: "
+      puts "Leads page found:"
+      puts line
       website = doc.xpath("//h4[@class=\"link\"]/a/@href").to_s()
-      puts website
       if website != ""
+        puts "Leads page website:"
+        puts website
+
         csv << [website, row[1]]
         allowed_domains += "\'" + website.split("http://")[-1].split("www.")[-1].split("/")[0] + "\', "
         start_urls += "\'" + website + "\', "
-        puts ""
+        
+#        add_to_csv(csv, website, row[1], allowed_domains, start_urls)
+   
       end
+      puts ""
     else
       puts "Not a leads page."
       puts ""
