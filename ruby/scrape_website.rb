f2c5ac40d30d48a9e6bd960d39ee68426fe24a92
kng-linux <kng@raker.sea>
2014-11-11 11:54:27 -0800

    added stop_crawl() method to anemone

--- ruby/scrape_website.rb
+++ ruby/scrape_website.rb
@@ -11,21 +11,28 @@ keyword_list2 = ["nail bomb", "chemical bomb", "firework", "firecracker", "grena
 keyword_found = false
 
 csv_contents = CSV.read("websites.csv")
-csv_contents.each do |row|
-  line = row[0]
+csv_contents = [['http://www.rozziproducts.com', '41-test']]
+
+CSV.open("results.csv", "wb") do |csv|
+  csv_contents.each do |row|
+    line = row[0]
   
-  keyword_found = false
-  Anemone.crawl(line) do |anemone|
-    anemone.on_every_page do |page|
-      unless keyword_found
-        puts page.url
-        html = page.body.downcase
-         if keyword_list.any?{ |k| html.include?(k) }
-          puts "Keyword found on: " + page.url.to_s
-          keyword_found = true
+    keyword_found = false
+    Anemone.crawl(line) do |anemone|
+      anemone.on_every_page do |page|
+        unless keyword_found
+          url =  page.url
+          html = page.body.downcase
+          if keyword_list2.any?{ |k| html.include?(k) }
+            puts "Keyword found on: " + url.to_s
+            keyword_found = true
+            csv << [url, row[1]]
+          end
+        else
+          anemone.stop_crawl()
         end
       end
+      
     end
-
   end
 end
